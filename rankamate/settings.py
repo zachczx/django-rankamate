eb2c9fba80df4c68cf3aeaa6006e883fbf3ccfb3
zachczx <zixian.chen@gmail.com>
2024-07-06 23:39:37 +0800

    Docker

--- rankamate/settings.py
+++ rankamate/settings.py
@@ -97,9 +97,17 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": "postgres",
+    "USER": "postgres",
+    "PASSWORD": "postgres",
+    "HOST": "db",  # set in docker-compose.yml
+    "PORT": 5432,  # default postgres port
     }
 }
 
